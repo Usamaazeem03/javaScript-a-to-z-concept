f4c5c1843d8ff1b57145ae27c7f495f2c91f0534
Usama <usamakha2003@gmail.com>
2025-08-23 17:15:51 +0500

    comments(single line, multi line)

---
+++ Basic-&-Fundamentals/03-comments.js
@@ -0,0 +1,21 @@
+//  comments(single line, multi line)
+//  comments are not executed by javascript engine
+//  comments are used to explain the code, make it more readable and understandable
+//  comments are also used to prevent execution, when testing alternative codes
+//  comments are also used to debug the code
+//  comments are also used to disable code temporarily, when testing alternative codes
+/*  multi line comments
+    multi line comments are used to explain the code, make it more readable and understandable
+    multi line comments are also used to prevent execution, when testing alternative codes
+    multi line comments are also used to debug the code
+    multi line comments are also used to disable code temporarily, when testing alternative codes
+*/
+/*  multi line comments
+    multi line comments are used to explain the code, make it more readable and understandable
+    multi line comments are also used to prevent execution, when testing alternative codes
+    multi line comments are also used to debug the code 
+    multi line comments are also used to disable code temporarily, when testing alternative codes
+*/
+
+//  single line comment
+console.log("Hello World"); //  single line comment
