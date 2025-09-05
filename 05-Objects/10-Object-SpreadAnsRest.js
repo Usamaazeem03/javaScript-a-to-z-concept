56adeec2d1a011fb822d9d0908b4d4c14ad8c7bc
Usama <usamakha2003@gmail.com>
2025-09-05 17:27:01 +0500

    Object Spread and Rest 🌀

--- 05-Objects/10-Object-SpreadAnsRest.js
+++ 05-Objects/10-Object-SpreadAnsRest.js
@@ -41,3 +41,33 @@ console.log(newCar);
 // speed: "220km/h"
 
 // Rest
+const student = {
+  name: "Usama",
+  age: 22,
+  grade: "4.5",
+  city: "Karachi",
+};
+// Directly
+const { name, age, ...another } = student;
+console.log("Directly Remaining properties:", another);
+console.log("Directly Name and Age:", name, age);
+
+// Inside function
+function remaProp({ name, age, ...another }) {
+  console.log("Remaining properties:", another);
+  console.log("Name and Age:", name, age);
+}
+remaProp(student);
+
+// Summary ✨
+// 1. Object Spread
+// . You used (...) to merge two objects like(birdDertails + birdHabitat) into a new object parratInfo.
+// . You also showed how to override existing properties (brand, name) when spreading int a new object newCar.
+
+// 2. Object Rest
+// . You used rest(...another) to collect the remaining properties of an object after extractingsome (name, age),
+// . You demonstrated this both directly with destructuring and inside a function.
+
+// ✅ This file clearly explains how Spread and Rest work with objects:
+// . Spread(...)--> Copy/merge/override object properties.
+// . Rest(...)--> Extract the remaining properties into a new object.
